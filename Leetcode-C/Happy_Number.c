949e41d2d5346e48477617b6120a05b236620c17
Karan <karanraj2006rk@gmail.com>
2026-08-17 15:34:13 +0530

    feat: add Happy Number solution in C

---
+++ Leetcode-C/Happy_Number.c
@@ -0,0 +1,18 @@
+int getNext(int n) {
+    int totalSum = 0;
+    while (n > 0) {
+        int d = n % 10;
+        totalSum += d * d;
+        n = n / 10;
+    }
+    return totalSum;
+}
+bool isHappy(int n) {
+    int slow = n;
+    int fast = getNext(n);
+    while (fast != 1 && slow != fast) {
+        slow = getNext(slow);
+        fast = getNext(getNext(fast));
+    }
+    return fast == 1;
+}
